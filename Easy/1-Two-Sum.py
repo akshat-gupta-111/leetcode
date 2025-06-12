2f5391f47874c4730ac317f096a667f58aee280e
AKSHAT GUPTA <akshatgupta111@AKSHATs-MacBook-Air-2.local>
2025-06-12 19:35:04 +0530

    Radhe-Radhe

---
+++ Easy/1-Two-Sum.py
@@ -0,0 +1,32 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+https://leetcode.com/problems/two-sum/description/
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+'''
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+
+        for i in range(len(nums)):
+            difference = target - nums[i]
+            if difference in seen:
+                return [i,seen[difference]]
+            else:
+                seen[nums[i]] = i
