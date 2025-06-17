72c60efbeb7d8ce9581a6005ac59a804d20c2fd5
AKSHAT GUPTA <akshatguptamtr@gmail.com>
2025-06-17 12:31:29 +0530

    day-6

--- Medium/853-Car-Fleet.py
+++ Medium/853-Car-Fleet.py
@@ -19,7 +19,7 @@ Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
 
 Output: 3
 
-Explanation:
+Explanation :
 
 The cars starting at 10 (speed 2) and 8 (speed 4) become a fleet, meeting each other at 12. The fleet forms at target.
 The car starting at 0 (speed 1) does not catch up to any other car, so it is a fleet by itself.
